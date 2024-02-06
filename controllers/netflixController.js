6254c74cbe911f42244827b3c901370686749b5a
Nixon Lim <limnixon@ibm.com>
2024-02-07 04:50:57 +0800

    change model filename

--- controllers/netflixController.js
+++ controllers/netflixController.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const NetflixData = require('../models/ENetflix_data.js');
-const MyListData = require('../models/Mylist_data');
+const MyListData = require('../models/EMylist_data.js');
 
 exports.saveTimeStamp = async (req,res) => {
     const { email, movieId, timestamp } = req.body;
