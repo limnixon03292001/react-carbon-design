62795b934805096109fce9f3484fe3998fce6aac
Nixon Lim <limnixon@ibm.com>
2024-02-07 04:58:30 +0800

    final update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const path = require('path');
 
 const netflixRoute = require('./routes/netflixRoute');
 
@@ -18,8 +19,17 @@ mongoose.connect(process.env.DB_URI, {
     useUnifiedTopology: true,
 });
 
-app.use('/netflix', netflixRoute); //login user
+app.use('/netflix', netflixRoute);
+
+if(process.env.NODE_ENV === "production") {
+    // app.use(express.static(path.join(__dirname, "client/build")));
+
+    app.use(express.static("client/build"));
+    app.get("*", (req,res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
 
 const server = app.listen(PORT, () => {
-    console.log(`The server is running at localhost ${PORT}`)
+    console.log(`The server is running.`)
 });
